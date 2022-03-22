258f12ad71ee19650e5cdaca5ed508c69526332e
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-22 13:47:37 +0700

    feat: move App / absolute import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
 import styled from "styled-components/macro";
-import "./App.css";
-
 function App() {
   return <Wrapper>Nitad18 NFT Website</Wrapper>;
 }
 const Wrapper = styled.div`
-  background: var(--color-turqoise);
+  background: red;
+  height: 100%;
 `;
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { App } from "src/app";
+import { nitad18Background } from "src/assets";
 import GlobalStyles from "./ui/GlobalStyles";
 
 ReactDOM.render(
