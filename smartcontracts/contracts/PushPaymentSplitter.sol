6e21e307f5c32b6dedb5972359480fcb79d4089f
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-24 22:22:37 +0700

    feat: add addTargets / update modifier to external

--- smartcontracts/contracts/PushPaymentSplitter.sol
+++ smartcontracts/contracts/PushPaymentSplitter.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 // AUTHOR: yoyoismee.eth
+// Modified by: mickyngub 
 
 pragma solidity ^0.8.4;
 
@@ -19,12 +20,19 @@ contract PushPaymentSplitter is Ownable, ReentrancyGuard {
     receive() external payable {}
     fallback() external payable {}
     // @notice add new recipient
-    function addTarget(address recipient) public onlyOwner {
+    function addTarget(address recipient) external onlyOwner {
         recipients.push(recipient);
     }
 
+    // @notice add new recipients
+    function addTargets(address[] memory _recipients) external onlyOwner {
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            recipients.push(_recipients[i]);
+        }
+    }
+
     // @notice push balance to all recipients.
-    function split() public onlyOwner nonReentrant {
+    function split() external onlyOwner nonReentrant {
         uint256 amount = address(this).balance / recipients.length;
         for (uint256 i = 0; i < recipients.length; i++) {
             payable(recipients[i]).transfer(amount);
