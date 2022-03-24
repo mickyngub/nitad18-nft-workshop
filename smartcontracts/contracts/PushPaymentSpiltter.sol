4f082433568ef743c97df54553d49b58b53a36be
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-24 20:33:47 +0700

    feat: bump solidity version

--- smartcontracts/contracts/PushPaymentSpiltter.sol
+++ smartcontracts/contracts/PushPaymentSpiltter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // AUTHOR: yoyoismee.eth
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
