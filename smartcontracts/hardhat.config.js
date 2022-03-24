3661302ed2ad2b73b953075092e008e74e5e91cf
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-24 20:48:11 +0700

    feat: add Matic Mumbai and Matic Mainnet

--- smartcontracts/hardhat.config.js
+++ smartcontracts/hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +18,31 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    maticMumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.POLYGON_MUMBAI_ALCHEMY_API}`,
+      accounts:
+        process.env.NITAD18_PRIVATE_KEY !== undefined
+          ? [process.env.NITAD18_PRIVATE_KEY]
+          : [],
+    },
+    maticMainnet: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.POLYGON_MAINNET_ALCHEMY_API}`,
+      accounts:
+        process.env.NITAD18_PRIVATE_KEY !== undefined
+          ? [process.env.NITAD18_PRIVATE_KEY]
+          : [],
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
