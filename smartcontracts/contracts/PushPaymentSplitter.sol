3a808ad76dca6e01efd7d650d61229522dd059ef
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-25 00:21:19 +0700

    feat: update PushPaymentSplitter V2

--- smartcontracts/contracts/PushPaymentSplitter.sol
+++ smartcontracts/contracts/PushPaymentSplitter.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // AUTHOR: yoyoismee.eth
-// Modified by: mickyngub 
+// Modified by: mickyngub / palsp
 
 pragma solidity ^0.8.4;
 
@@ -9,27 +9,15 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 // @notice just a push payment splitter. not the best practice. use it on trusted address only
 contract PushPaymentSplitter is Ownable, ReentrancyGuard {
-    address[] public recipients;
 
     receive() external payable {}
     fallback() external payable {}
-    // @notice add new recipient
-    function addTarget(address recipient) external onlyOwner {
-        recipients.push(recipient);
-    }
 
-    // @notice add new recipients
-    function addTargets(address[] memory _recipients) external onlyOwner {
+    // @notice receive addresses and push balance to all recipients.
+    function split(address[] memory _recipients) external onlyOwner nonReentrant {
+        uint256 amount = address(this).balance / _recipients.length;
         for (uint256 i = 0; i < _recipients.length; i++) {
-            recipients.push(_recipients[i]);
-        }
-    }
-
-    // @notice push balance to all recipients.
-    function split() external onlyOwner nonReentrant {
-        uint256 amount = address(this).balance / recipients.length;
-        for (uint256 i = 0; i < recipients.length; i++) {
-            payable(recipients[i]).transfer(amount);
+            payable(_recipients[i]).transfer(amount);
         }
     }
 }
