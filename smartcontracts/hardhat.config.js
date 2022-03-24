82f513e4a75fbd0ad8029df3e949eeb1fb415f5d
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-24 22:49:36 +0700

    feat: add @nomiclabs/hardhat-etherscan for contract verification

--- smartcontracts/hardhat.config.js
+++ smartcontracts/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -35,6 +36,9 @@ module.exports = {
           ? [process.env.NITAD18_PRIVATE_KEY]
           : [],
     },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
   solidity: {
     version: "0.8.4",
