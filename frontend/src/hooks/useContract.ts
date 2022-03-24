6682696fc46dd2693a485d0fa328735a39b6589a
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-25 02:31:59 +0700

    feat: update contract address

--- frontend/src/hooks/useContract.ts
+++ frontend/src/hooks/useContract.ts
@@ -3,7 +3,7 @@ import { Contract } from "@ethersproject/contracts";
 import { ethers } from "ethers";
 
 const useContract = () => {
-  const NFT_ADDRESS = "0x44f964aC98355aE48f64bf4BC5db81d6d485D8Fb";
+  const NFT_ADDRESS = "0x3A53ed558ABe74cDb12032097628B7c330C36d88";
   const NFT_INTERFACE = new ethers.utils.Interface(NFT_ABI);
   const NFT_CONTRACT = new Contract(NFT_ADDRESS, NFT_INTERFACE);
   return {
